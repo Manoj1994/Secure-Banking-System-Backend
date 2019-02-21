0b746bc18d3770920ac421d82c24ef4d81ace8f2
manoj yaramsetty <manojys1994@gmail.com>
2019-02-20 20:30:58 -0700

    new changes

--- src/main/java/com/bankingapp/model/User.java
+++ src/main/java/com/bankingapp/model/User.java
@@ -1,6 +1,58 @@
 package com.bankingapp.model;
 
+
+import jdk.jfr.DataAmount;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user")
 public class User {
 
-    
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getUserName(), user.getUserName()) &&
+                Objects.equals(getPassword(), user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserName(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/com/bankingapp/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.bankingapp.repository;
+
+public class UserRepository {
+}
